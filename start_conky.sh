ba7e87478bad9045ca56e02c732851abe4442f74
Patrick Hofmann <github@pahofmann.com>
2018-05-10 10:13:42 +0200

    Adjusted Path

--- start_conky.sh
+++ start_conky.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-nohup conky -c /home/patrick/.config/conky/conkyrc_clock &
-nohup conky -c /home/patrick/.config/conky/conkyrc_disk &
-nohup conky -c /home/patrick/.config/conky/conkyrc_net &
-nohup conky -c /home/patrick/.config/conky/conkyrc_sys &
+sleep 5
+nohup conky -c /home/patrick/.config/conky/conky-arch-dark/conkyrc_clock &
+nohup conky -c /home/patrick/.config/conky/conky-arch-dark/conkyrc_disk &
+nohup conky -c /home/patrick/.config/conky/conky-arch-dark/conkyrc_net &
+nohup conky -c /home/patrick/.config/conky/conky-arch-dark/conkyrc_sys &
