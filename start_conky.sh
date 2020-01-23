7b354635064edeaf9553f65876f99ab97f282860
pahofmann <git@hfmnn.com>
2020-01-23 02:36:51 +0100

    Added wireguard to network

--- start_conky.sh
+++ start_conky.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm -r /home/patrick/.config/conky/nohup.out
+sleep 10
 nohup conky -c /home/patrick/.config/conky/conkyrc_clock &
 nohup conky -c /home/patrick/.config/conky/conkyrc_disk &
 nohup conky -c /home/patrick/.config/conky/conkyrc_net &
